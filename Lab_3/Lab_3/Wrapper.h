cd67b4b79593255c2ea3b9e208a67c147357ac99
Lo-Lap <lapina.ok@edu.spbstu.ru>
2023-12-01 14:01:12 +0300

    Editing Wrapper without specifying the template class settings in c++17 standard

--- Lab_3/Lab_3/Wrapper.h
+++ Lab_3/Lab_3/Wrapper.h
@@ -5,66 +5,64 @@
 #include <map>
 #include <vector>
 
-
+template <typename ObjectType, typename ...Args>
 class Wrapper
 {
 private:
-	template <typename ObjectType, typename ...Args>
+	ObjectType* Class; //save class object
+	int(ObjectType::* func_method)(Args...);  //save the transferred class method
 
-	//структура дл€ храненни€ шаблонного экземпл€ра класса и метода функции
-	struct cl_
+	std::map <std::string, int> map_args; //here we save the dictionary of arguments
+	// when you initialize a dictionary, we determine the order of arguments, 
+	// such as entered when creating Wrapper object
+
+	std::vector<int> values_of_args; //the arguments are stored in the correct order (according to the key order of the map_args dictionary) 
+	//then func_method will be applied to the values of this vector
+
+
+	// function to decompress vector into Args argument list and call func_method function 
+	template<std::size_t... S>
+	int calculation_value(std::index_sequence<S...>)
 	{
-		ObjectType* Class;
-		int(ObjectType::* func_method)(Args...);
+		//values_of_args[S]... unzips vector in Args
+		//(Class->*(func_method) call method passed class
+		return (Class->*(func_method))(values_of_args[S]...);
 	};
-	
-	std::function<int(std::map<std::string, int>)> func_calculation;
-
 
 public:
-	
-	std::map <std::string, int> map_args;
-	std::vector<int> values_of_args;
 
-	template <typename ObjectType, typename ...Args>
 	Wrapper(ObjectType* my_class, int(ObjectType::* Function)(Args...), std::map <std::string, int> arg_map_from)
 	{
-		cl_<ObjectType, Args...> class_func;
-		class_func.Class = my_class;
-		class_func.func_method = Function;
+		Class = my_class;
+		func_method = Function;
 		map_args = arg_map_from;
 
-		//link method function with dictionary arguments
-		func_calculation = [class_func, this](std::map<std::string, int> args_map_to_cal)
+	};
+
+	//link method function with dictionary arguments
+	std::function<int(std::map<std::string, int>)> func_calculation = [this](std::map<std::string, int> args_map_to_cal)
+		{
+			if (args_map_to_cal.size() != map_args.size())
 			{
-				if (args_map_to_cal.size() != this->map_args.size())
+				throw std::exception();
+			}
+
+			for (auto& elem_args : args_map_to_cal)
+			{
+				if (map_args.find(elem_args.first) == map_args.end())
+					//did not find such a key in the dictionary initialized when creating the Wrapper object
 				{
 					throw std::exception();
 				}
 
-				for (auto& elem_args : args_map_to_cal)
-				{
-					if (this->map_args.find(elem_args.first) == this->map_args.end()) //не нашли такой аргумент в изначальном
-					{
-						throw std::exception();
-					}
+				values_of_args.push_back(elem_args.second);
+			}
 
-					this->values_of_args.push_back(elem_args.second);
-				}
+			//std::make_index_sequence<sizeof ...(Args)>() is needed to create an index sequence 
+			//elements of the argument value vector to transform the vector into a list of function arguments in the called function
+			return calculation_value(std::make_index_sequence<sizeof ...(Args)>());
+		};
 
-				//std::make_index_sequence<sizeof ...(Args)>() is needed to create an index sequence 
-				//elements of the argument value vector to transform the vector into a list of function arguments in the called function
-				return this->calculation_value<ObjectType, Args...>(class_func, std::make_index_sequence<sizeof ...(Args)>());
-			};
-	};
 
-	// function to decompress vector into Args argument list and call func_method function 
-	template<typename ObjectType, typename ...Args, std::size_t... S>
-	int calculation_value(cl_<ObjectType, Args...>  class_func, std::index_sequence<S...>)
-	{
-		//values_of_args[S]... unzips vector in Args
-		//(class_func.Class)->*(class_func.func_method) call method passed class
-		return ((class_func.Class)->*(class_func.func_method))(values_of_args[S]...);
-	};
-	
+
 };
